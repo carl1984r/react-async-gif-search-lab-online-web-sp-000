bf4cefa8947869c26df9a8258615224c3b05040a
Bry Cox <carl.cc@gmail.com>
2020-04-04 08:30:47 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -4,9 +4,9 @@ class GifList extends Component{
 
     render() {
         return (
-        <ul>
-         {this.props.gifs.map(gif => <li><img src={gif.images.original.url}/></li>)}
-        </ul>
+          <ul>
+            {this.props.gifs.map(gif => <li><img src={gif.images.original.url}/></li>)}
+          </ul>
         )}
 }
 
